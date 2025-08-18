72c380e9b451eb4bb0ca46310ada3c3fd9e087fb
Subhajit Kar <subhajitkar19@gmail.com>
2025-08-18 12:14:44 +0300

    Update test_checkpoint.jl

--- test/test_checkpoint.jl
+++ test/test_checkpoint.jl
@@ -1,15 +1,15 @@
 @testset "Checkpoint JLD2" begin
-    dom = VortexMethod.default_domain()
+    domain = VortexMethod.default_domain()
     gr = VortexMethod.default_grid()
     Nx, Ny = 8, 8
-    nodeX, nodeY, nodeZ, tri, triXC, triYC, triZC = VortexMethod.structured_mesh(Nx, Ny; dom=dom)
+    nodeX, nodeY, nodeZ, tri, triXC, triYC, triZC = VortexMethod.structured_mesh(Nx, Ny; domain=domain)
     eleGma = zeros(Float64, size(tri,1), 3)
     tmpfile = joinpath("checkpoints", "test_series.jld2")
     isdir(dirname(tmpfile)) || mkpath(dirname(tmpfile))
     base1 = VortexMethod.save_state_timeseries!(tmpfile, 0.0, nodeX, nodeY, nodeZ, tri, eleGma;
-        dom=dom, grid=gr, step=1)
+        domain=domain, grid=gr, step=1)
     base2 = VortexMethod.save_state_timeseries!(tmpfile, 0.1, nodeX, nodeY, nodeZ, tri, eleGma;
-        dom=dom, grid=gr, step=2)
+        domain=domain, grid=gr, step=2)
     times, steps, count = VortexMethod.series_times(tmpfile)
     @test count == 2
     @test steps == [1,2]
@@ -17,4 +17,3 @@
     @test idx == 2
     @test size(snap.tri,2) == 3
 end
-
