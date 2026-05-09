4df2a40268d97c3fffc56fc9fe6b256e37822124
Subhajit Kar <subhajitkar19@gmail.com>
2026-05-09 15:34:56 +0300

    updates

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,10 +23,12 @@ end
     include("test_parallel_fft.jl")
     include("test_performance.jl")
 
-    @testset "MPI parallel correctness launcher" begin
+    if get(ENV, "VORTEXMETHOD_RUN_MPI_TESTS", "false") == "true"
         mpi_test = joinpath(@__DIR__, "mpi_parallel_correctness.jl")
         repo_root = dirname(@__DIR__)
         cmd = `$(MPI.mpiexec()) -n 2 $(Base.julia_cmd()) --project=$repo_root $mpi_test`
-        @test success(cmd)
+        @testset "MPI parallel correctness launcher" begin
+            @test success(cmd)
+        end
     end
 end
